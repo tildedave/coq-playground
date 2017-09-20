258d79ece676c3a503c1d226281f78f053a1c7de
Dave King <tildedave@gmail.com>
2017-09-19 17:09:21 -0700

    Add proof of intersection commutative/associative

--- test.v
+++ test.v
@@ -1,5 +1,17 @@
 Require Import List Ensembles.
 
+Section minimal_logic.
+
+
+Theorem False_cannot_be_proven : ~False.
+Proof.
+    unfold not.
+    intros H.
+    case H.
+Qed.
+
+End minimal_logic.
+
 Section test_sets.
 
 (* A included in B and B included in C implies A included in C *)
@@ -18,3 +30,34 @@ Proof.
     apply a_in_b.
     apply a_in_x.
 Qed.
+
+Theorem Intersection_Commutes : Included U (Intersection U A B) (Intersection U B A).
+Proof.
+    intro x.
+    intro x_in_a_b.
+    elim x_in_a_b.
+    intros.
+    split.
+    assumption.
+    assumption.
+Qed.
+
+(* (A ∩ B) ∩ C = A ∩ (B ∩ C) *)
+
+Theorem Intersection_Assoc : Included U (Intersection U (Intersection U A B) C) (Intersection U A (Intersection U B C)).
+Proof.
+    intro x.
+    intro x_in_a_b_then_c.
+    elim x_in_a_b_then_c.
+    intros x0 x0_in_a_intersect_b.
+    split.
+    elim x0_in_a_intersect_b.
+    intros.
+    assumption.
+    split.
+    elim x0_in_a_intersect_b.
+    intros.
+    assumption.
+    assumption.
+Qed.
+
