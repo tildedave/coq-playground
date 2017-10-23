538073303aead11bf21b8b16778882e55441e1bd
Dave King <tildedave@gmail.com>
2017-10-22 17:34:24 -0700

    Remove redundant >=0 condition
    Always true for nat

--- digits.v
+++ digits.v
@@ -2,10 +2,10 @@ Require Import List Arith BinNat.
 Import ListNotations.
 
 Inductive digit : Set :=
-| Digit : forall n, n >= 0 -> n < 10 -> digit.
+| Digit : forall n, n < 10 -> digit.
 
 Fixpoint denoteDigit (d : digit) : nat :=
-  match d with Digit n _ _ => n end.
+  match d with Digit n _ => n end.
 
 Definition denotePair p :=
   match p with
@@ -16,53 +16,53 @@ Definition denoteDigitList (dl : list digit) : nat :=
   fold_left (fun n d => (n * 10) + (denoteDigit d)) dl 0.
 
 Definition D0 : digit.
-  refine (Digit 0 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 0 _).
+  auto with arith.
 Defined.
 
 Definition D1 : digit.
-  refine (Digit 1 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 1 _).
+  auto with arith.
 Defined.
 
 Definition D2 : digit.
-  refine (Digit 2 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 2 _).
+  auto with arith.
 Defined.
 
 Definition D3 : digit.
-  refine (Digit 3 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 3 _).
+  auto with arith.
 Defined.
 
 Definition D4 : digit.
-  refine (Digit 4 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 4 _).
+  auto with arith.
 Defined.
 
 Definition D5 : digit.
-  refine (Digit 5 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 5 _).
+  auto with arith.
 Defined.
 
 Definition D6 : digit.
-  refine (Digit 6 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 6 _).
+  auto with arith.
 Defined.
 
 Definition D7 : digit.
-  refine (Digit 7 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 7 _).
+  auto with arith.
 Defined.
 
 Definition D8 : digit.
-  refine (Digit 8 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 8 _).
+  auto with arith.
 Defined.
 
 Definition D9 : digit.
-  refine (Digit 9 _ _).
-  auto with arith.  auto with arith.
+  refine (Digit 9 _).
+  auto with arith.
 Defined.
 
 Compute (denoteDigitList [D3 ; D4 ; D7]).
@@ -71,7 +71,7 @@ Definition toDigit (n : nat | n >= 0 /\ n < 10) : digit.
   inversion n.
   elim H.
   intros.
-  exact (Digit x H0 H1).
+  exact (Digit x H1).
 Defined.
 
 Lemma all_nat_gte_zero : forall m, m >= 0.
@@ -100,8 +100,8 @@ Definition addDigit (d1 : digit) (d2 : digit) : (digit * digit).
   remember ((m + n) / 10) as remainder.
   assert (total < 10) as TotalLt10. rewrite Heqtotal. apply mod_is_lt. auto with arith.
   assert (remainder < 10) as RemainderLt10. rewrite Heqremainder. apply div_is_lt.
-  exact (Digit remainder (all_nat_gte_zero remainder) RemainderLt10,
-         Digit total (all_nat_gte_zero total) TotalLt10).
+  exact (Digit remainder RemainderLt10,
+         Digit total TotalLt10).
 Defined.
 
 Theorem addDigit_works_as_expected :
