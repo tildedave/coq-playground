e25aae0e68ade1731a56559bf576c0ec4e47b50a
Dave King <tildedave@gmail.com>
2018-07-29 11:12:28 -0700

    Prime divisors - getting somewhere!

--- primes.v
+++ primes.v
@@ -28,107 +28,82 @@ Fixpoint find_factor_helper (m n i : nat) : nat :=
               else find_factor_helper (m + 1) n p)
   end.
 
-Fixpoint find_factor (n : nat) : nat := find_factor_helper 2 n n.
+Definition find_factor (n : nat) : nat := find_factor_helper 2 n n.
 
-(*
-Program Fixpoint factors (n : Z) (H : n > 0) : list Z :=
-  find_factor n n H
- *)
-
-Lemma a_mod_nonzero : forall a b, a mod b <> 0 -> b <> 0 -> a <> 0.
-  intros a b AModB_NotZero BNotZero.
-  Search (?a <> 0 -> _ mod _ = _).
-  rewrite Nat.mod_eq in AModB_NotZero.
-  assert (b > 0). omega.
-  unfold not.
-  intros AIsZero.
-  rewrite AIsZero in AModB_NotZero.
-  simpl in AModB_NotZero.
-  contradict AModB_NotZero.
-  auto.
-  auto.
+Lemma a_mod_a_eq_0: forall a, a mod a = 0.
+Proof.
+  intros; auto.
+  destruct (Nat.eq_dec a 0). rewrite e; simpl; auto.
+  apply Nat.mod_same in n; auto.
 Qed.
 
-Fixpoint find_factor (m : nat) (n : nat) : nat :=
-  match m with
-    0 => n
-  | 1 => n
-  | (S p) => (if (Nat.eq_dec (n mod m) 0) then
-                m
-              else find_factor p n)
-  end.
+Lemma find_factor_helper_mult : forall i m n b, 1 < m -> (find_factor_helper m n i) = b -> n mod b = 0.
+  induction i.
+  intros m n b m_gt_1.
+  simpl.
+  intros n_eq_b; rewrite n_eq_b.
+  apply a_mod_a_eq_0.
+  intros m n b m_gt_1.
+  intros FindFactor.
+  unfold find_factor_helper in FindFactor.
+  destruct (Nat.eq_dec i 0) as [i_eq_0 | i_neq_0].
+  rewrite i_eq_0 in FindFactor.
+  rewrite FindFactor; apply a_mod_a_eq_0.
+  destruct (Nat.eq_dec (n mod m) 0) as [n_mod_m_eq_0 | n_mod_m_neq_0].
+  destruct i in FindFactor.
+  rewrite <- FindFactor; apply a_mod_a_eq_0.
+  rewrite <- FindFactor; auto.
+  fold find_factor_helper in FindFactor.
+  remember (find_factor_helper (m + 1) n i) as c.
+  symmetry in Heqc.
+  assert (m + 1 > 1). Search (_ + _ > _). omega.
+  apply (IHi _ _ _ H) in Heqc.
+  destruct i in FindFactor.
+  rewrite FindFactor. apply a_mod_a_eq_0.
+  rewrite <- FindFactor; auto.
+Qed.
 
-Compute (find_factor 12 15).
+Lemma find_factor_helper_1_lt : forall i m n b, 1 < n -> 1 < m -> (find_factor_helper m n i) = b -> 1 < b.
+  induction i.
+  intros m n b n_neq_0 m_gt_0 FindFactor.
+  simpl in FindFactor.
+  rewrite <- FindFactor; auto.
+  intros m n b n_neq_0 m_gt_0 FindFactor.
+  unfold find_factor_helper in FindFactor.
+  destruct (Nat.eq_dec (n mod m) 0) as [n_mod_m_eq_0 | n_mod_m_neq_0].
+  destruct i in FindFactor.
+  rewrite <- FindFactor; auto.
+  rewrite <- FindFactor. auto.
+  fold find_factor_helper in FindFactor.
+  remember (find_factor_helper (m + 1) n i) as c.
+  inversion FindFactor.
+  symmetry in Heqc.
+  assert (1 < m + 1) as One_lt_m_plus. auto with arith.
+  apply (IHi _ _ _ n_neq_0 One_lt_m_plus) in Heqc.
+  destruct i; auto; auto.
+Qed.
 
-Theorem find_factor_mult : forall a n b, n <> 0 -> (find_factor a n) = b -> n mod b = 0.
-  intros a n b N_NonZero.
-  induction a.
-  intro FindFactor.
-  simpl in FindFactor; rewrite <- FindFactor.
-  apply Nat.mod_same in N_NonZero; auto.
+Theorem find_factor_mult: forall n b, 1 < n -> find_factor n = b -> exists c, c * b = n.
+  intros n b n_lt_1.
   intros FindFactor.
   unfold find_factor in FindFactor.
-  destruct (Nat.eq_dec (n mod S a) 0) as [H_Sa_Divides | H_Sa_NotDivides].
-  destruct a.
-  apply Nat.mod_same in N_NonZero; auto.
-  rewrite FindFactor in H_Sa_Divides; auto.
-  fold find_factor in FindFactor.
-  destruct a.
-  apply Nat.mod_same in N_NonZero; auto.
-  apply IHa in FindFactor; auto.
+  assert (1 < 2) as H1_lt_2; auto.
+  assert (b <> 0) as b_neq_0.
+  apply (find_factor_helper_1_lt _ _ _ _ n_lt_1 H1_lt_2) in FindFactor; omega.
+  apply (find_factor_helper_mult _ _ _ _ H1_lt_2) in FindFactor.
+  apply (Nat.mod_divides _ _ b_neq_0) in FindFactor.
+  destruct FindFactor as [c' H2].
+  exists c'.
+  rewrite Nat.mul_comm.
+  auto.
 Qed.
 
-Lemma cut : forall j k : nat, j <> 0 -> j mod k = 0 -> j > k.
+Theorem find_factor_1_lt : forall n b, 1 < n -> find_factor n = b -> 1 < b.
 Proof.
-  intros j k.
-  intros J_NonZero J_ModK_Zero.
-  Search (_ mod _ = 0).
-  destruct k.
-  apply Nat.neq_0_lt_0 in J_NonZero; auto.
-  assert (S k <> 0); auto with arith.
-  apply (Nat.mod_divides j (S k) H) in J_ModK_Zero.
-  destruct J_ModK_Zero as [q H1].
-  assert (j > 0 /\ S k > 0). omega.
-  rewrite H1 in H0.
-  unfold gt in H0.
-  destruct H0.
-  Search (_ < _ * _).
-  apply Nat.lt_0_mul' in H0.
-  destruct H0.
-  unfold gt.
-  rewrite H1.
-  Search (_ < _ * _).
-Admitted.
-
-Theorem find_factor_decreasing : forall a b n, n <> 0 -> (find_factor a n) = b -> b <= n.
-Proof.
-  intros a b n N_NonZero.
-  induction a.
-  simpl; omega.
-  unfold find_factor.
-  fold find_factor.
-  destruct (Nat.eq_dec (n mod (S a)) 0) as [H_Sa_Divides | H_Sa_NotDivides].
-  assert (S a <> 0) as Sa_NonZero; auto.
-  apply (cut _ _ N_NonZero) in H_Sa_Divides.
-  intro.
-  destruct H.
-  destruct a; auto.
-  omega.
-  destruct a; auto.
-Qed.
-
-Theorem find_factor_gt_1 : forall a n, n > 1 -> find_factor a n > 1.
-Proof.
-  induction a.
-  intros n N_NonZero.
-  simpl; auto.
-  unfold find_factor.
-  fold find_factor.
-  intros n n_gt_1.
-  destruct (Nat.eq_dec (n mod (S a)) 0) as [sa_Divides | sa_NotDivides].
-  case a. auto. intros; omega.
-  destruct a. auto.
-  apply IHa in n_gt_1; auto.
+  intros n b H1_lt_n FindFactor.
+  unfold find_factor in FindFactor.
+  assert (1 < 2) as H1_lt_2; auto.
+  apply (find_factor_helper_1_lt n 2 n _ H1_lt_n H1_lt_2); auto.
 Qed.
 
 Require Import List.
@@ -138,24 +113,20 @@ Program Fixpoint prime_divisors (n : nat) { measure n } : (list nat) :=
   match n with
     0 => []
   | 1 => []
-  | (S p) => let a := find_factor p n in
+  | (S p) => let a := find_factor n in
              if (Nat.eq_dec a n) then
                [n]
              else
                a :: prime_divisors (n / a)
   end.
 Obligation 1.
-remember (find_factor p (S p)) as b.
-assert (S p > 1) as Sp_gt_1. omega.
-apply (find_factor_gt_1 p (S p)) in Sp_gt_1.
-rewrite <- Heqb in Sp_gt_1.
-unfold gt in Sp_gt_1.
+remember (find_factor (S p)) as b.
+assert (1 < S p) as H1_lt_Sp. omega.
+symmetry in Heqb.
+apply (find_factor_1_lt (S p) b H1_lt_Sp) in Heqb.
 assert (0 < S p).
 apply Nat.lt_0_succ.
-apply (Nat.div_lt _ _ H1 Sp_gt_1).
+apply (Nat.div_lt _ _ H1 Heqb).
 Defined.
 
-Compute (prime_divisors 16).
-
 Compute (prime_divisors 8).
-Compute (find_factor 8).
